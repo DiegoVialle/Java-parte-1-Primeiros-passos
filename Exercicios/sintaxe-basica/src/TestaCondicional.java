897a2fa1a9d37ad8ffc8488dddae574af9a71b35
Diego Vialle <57689631+DiegoVialle@users.noreply.github.com>
2020-02-12 17:09:28 -0300

    Resolução dos exercicios

---
+++ Exercicios/sintaxe-basica/src/TestaCondicional.java
@@ -0,0 +1,26 @@
+
+public class TestaCondicional {
+
+	public static void main (String[] args) {
+		System.out.println("testando condicionais");
+
+//		int idade = 20;
+//		int quantidadePessoas = 3;
+//
+//		if (idade >= 18) {
+//			System.out.println("Você tem mais que 18 anos");
+//			System.out.println("Seja bem vindo");
+//		}else {
+//			if (quantidadePessoas >= 2) {
+//				System.out.println("voce nao tem 18, mas pode entrar, pois está acompanhado");
+//			}
+//			System.out.println("Infelizmente voce nao pode entrar");
+//		}
+		int idade = 68;
+		boolean ehIdoso = idade >= 65;
+
+		System.out.println(ehIdoso);
+	}
+
+}
+
---
+++ Exercicios/sintaxe-basica/src/TestaVariaveis.java
@@ -0,0 +1,19 @@
+
+public class TestaVariaveis {
+	public static void main (String[] args) {
+		System.out.println("ola novo teste");
+		
+		int idade;
+		idade = 30;
+		
+		System.out.println("a idade é " + idade + ", parabéns !");
+		
+		int dia = 4;
+		int outroDia = 4 + dia;
+		System.out.println(outroDia);
+		
+		int brindes = 15;
+		int pessoas = 2;
+		System.out.println(brindes/pessoas);
+	}
+}
